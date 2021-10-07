9c628ad91f2029c5b463e7526d500a4938d7e23e
billy.ordores <billy.ordores@alumnos.uneatlantico.es>
2021-10-07 11:55:29 +0200

    version 3

---
+++ Ejercicio3/Ejercicio3/Arranque.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Arranque
+    {
+        static void Main(string[] args)
+        {
+            Entero newEntero = new Entero(20);
+            Console.WriteLine($"el cuadrado de 20 es {newEntero.CalcularCuadrado()} ");
+            Console.WriteLine($"el cubo de 20 es {newEntero.CalcularCubo()} ");
+
+        }
+    }
+}
---
+++ Ejercicio3/Ejercicio3/Entero.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+
+    class Entero
+    {
+        public int number;
+        public Entero(int numberTwo) {
+            this.number = numberTwo;
+        }
+
+        public int CalcularCubo() {
+            return (int)Math.Pow(number, 3);
+        }
+        public int CalcularCuadrado() {
+            return (int)Math.Pow(number, 2);
+        }
+    }
+}
