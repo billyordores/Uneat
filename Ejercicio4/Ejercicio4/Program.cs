4cb2eb8101078aa1e9a253abac965af4a9a984a8
billy.ordores <billy.ordores@alumnos.uneatlantico.es>
2021-10-08 09:58:23 +0200

    versionTwo

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int scanner = 0;
+            while (scanner!=3) {
+                
+                Console.WriteLine("*------Menu-----*");
+                Console.WriteLine("Opción 1-> MostrarPrimos");
+                Console.WriteLine("Opción 2-> MostrarNoPrimos");
+                Console.WriteLine("Opción 3-> Salir");
+                Console.Write("->");
+                scanner = Convert.ToInt32(Console.ReadLine());
+                switch (scanner) {
+                    case 1:
+                        Varios var = new Varios();
+                        var.MostrarPrimos();
+                        break;
+                    case 2:
+                        Varios var2 = new Varios();
+                        var2.MostrarNoPrimos();
+                        break;
+                }
+                if (scanner != 2 & scanner != 1 & scanner != 3) {
+                    Console.WriteLine("Opción no disponible");
+                }
+                Console.WriteLine("*----------------------*");
+
+            }
+        }
+    }
+}
---
+++ Ejercicio4/Ejercicio4/Varios.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    class Varios
+    {   
+        public void MostrarPrimos() {            
+            for (int i=2; i<=30;i++ ) {
+                if (EsPrimo(i)) {
+                    Console.WriteLine($"El numero {i} es primo");
+                }
+            }    
+        }
+        public void MostrarNoPrimos() {
+            Console.WriteLine("El numero 1 no es primo");
+            for (int i = 2; i <= 30; i++)
+            {
+                if (!EsPrimo(i))
+                {
+                    Console.WriteLine($"El numero {i} no es primo");
+                }
+            }
+        }
+
+        static bool EsPrimo(int numero)
+        {
+            for (int i = 2; i < numero; i++)
+            {
+                if ((numero % i) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
